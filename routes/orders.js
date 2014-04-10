26c42bf997974f3216f30a76a7bf49b7c97d63b3
Bochen Jin <bochenjin@BochentekiMacBook-Air.local>
2014-04-10 00:08:18 -0700

    insert to db

--- routes/orders.js
+++ routes/orders.js
@@ -8,48 +8,40 @@ var queryDB = require('../node_modules/queryDB');
 
 exports.placeOrder = function(req,res) {
 
-<<<<<<< HEAD
+
 
         preorders = req.body.preorders;
-=======
+
         var preorders = req.body.preorders;
->>>>>>> 8d765b505a3e951a6ff29cf970e3a60f096b6511
 
         var userInfo = preorders.userInfo;
 
-        var orderInfoArray= [];
+        var orderInfoArray= {};
 
         console.log("preorders=" + preorders.rows.length);
+
+    orderInfoArray.vouchersArray=[];
+
 
         for(var i = 0; i < preorders.rows.length-1; i++) {
 
             var order = {};
             var row = preorders.rows[i]
-            order.start= row.cell[0];
-            order.end= row.cell[1];
+            orderInfoArray.vouchersArray[i] = {};
 
-            order.type= row.cell[2];
-            order.validDate = row.cell[3];
-            order.time=row.cell[4];
-            //order.price = preorders.row[i].cell[3];
-<<<<<<< HEAD
-            order.amount=row.cell[5];
-            order.price = row.cell[6];
-            order.subtotal=row.cell[7];
-            order.offerId = row.
-            console.log(order);
-=======
-            order.order_status='1';
-            order.total_amount = row.cell[6];
+            orderInfoArray.vouchersArray[i].offerId = preorders.offers[i];
+            orderInfoArray.vouchersArray[i].skuId = preorders.routes[i];
+            orderInfoArray.vouchersArray[i].scheduleId = preorders.schedules[i];
+            console.log(preorders.schedules[i]);
+            orderInfoArray.vouchersArray[i].validDate = preorders.rows[i].cell[3];
+            orderInfoArray.vouchersArray[i].quantity = preorders.rows[i].cell[5];
 
->>>>>>> 8d765b505a3e951a6ff29cf970e3a60f096b6511
-            orderInfoArray[i]=order;
         }
+
+         orderInfoArray.totalAmount=preorders.total_amount;
+
+
         var result ={};
-        for(var i =0; i < orderInfoArray.length; i++) {
-
-            console.log(orderInfoArray[i]);
-        }
 
         var handleOrder=function(number) {
             if(number==0) {
